c70be3c36370392a645cf5bb875060b15b8f49cf
Yu Vitaqua fer Chronos <random.access.exomemory@gmail.com>
2023-06-27 19:36:46 +0100

    Update Initial Files

---
+++ src/main/java/io/github/yu_vitaqua_fer_chronos/minemachinery/mixin/TitleScreenMixin.java
@@ -0,0 +1,16 @@
+package io.github.yu_vitaqua_fer_chronos.minemachinery.mixin;
+
+import io.github.yu_vitaqua_fer_chronos.minemachinery.MineMachineryInit;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class TitleScreenMixin {
+	@Inject(method = "init", at = @At("TAIL"))
+	public void onInit(CallbackInfo ci) {
+		MineMachineryInit.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
